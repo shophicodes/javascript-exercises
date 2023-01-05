79e11a32451faee792c5425a83c33462682af77c
Filip Ivanisevic <87235094+shophicodes@users.noreply.github.com>
2023-01-05 20:02:33 +0100

    All exercises finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,35 +1,13 @@
-/*const removeFromArray = function(arr, ...itemsToRemove) {
-		let newArr = [];
-    for(let i = 0; i < arr.length; i++) {
-    	for(let j = 0; j < itemsToRemove.length; j++) {
-      	console.log(`${arr[i]} === ${itemsToRemove[j]}`);
-      	if(arr[i] === itemsToRemove[j]) {
-    			continue;
-    		}
-      	else {
-      		newArr.push(arr[i]);
-        	//console.log(arr[i]);
-      	}
-      }	
-    }
-    return newArr;
-};*/
-
 const removeFromArray = function(arr, ...itemsToRemove) {
-    let newArr = [];
-for(let j = 0; j < itemsToRemove.length; j++) {
+  for(let j = 0; j < itemsToRemove.length; j++) {
     for(let i = 0; i < arr.length; i++) {
-      console.log(`${arr[i]} === ${itemsToRemove[j]}`);
       if(arr[i] === itemsToRemove[j]) {
-            continue;
-        }
-      else {
-          newArr.push(arr[i]);
-        //console.log(arr[i]);
+        arr.splice(i, 1);
+        break;
       }
-  }	
-}
-return newArr;
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let toCelsius = (temperature - 32) * (5 / 9)
+  return Math.round(toCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let toFahrenheit = (temperature * 1.8) + 32;
+  return Math.round(toFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
